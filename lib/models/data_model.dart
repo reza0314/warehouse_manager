fc9bfbf290cf070aa672cf457a9f60773ea42e33
Mohammad Amin Sadat <reza13771223@gmail.com>
2022-11-30 09:20:13 +0330

    adding simple view of model

---
+++ lib/models/data_model.dart
@@ -0,0 +1,10 @@
+class DataModel {
+  final String code;
+  final double weight;
+  final DateTime time;
+  DataModel({
+    required this.code,
+    required this.time,
+    required this.weight,
+  });
+}
