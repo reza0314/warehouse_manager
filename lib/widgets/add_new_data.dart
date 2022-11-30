3f440dec996dffba7285cd8a2f0456e6ea64a1dd
Mohammad Amin Sadat <reza13771223@gmail.com>
2022-11-30 09:55:51 +0330

    add file add_new_data

---
+++ lib/widgets/add_new_data.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewData extends StatefulWidget {
+  const NewData({super.key});
+
+  @override
+  State<NewData> createState() => _NewDataState();
+}
+
+class _NewDataState extends State<NewData> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
